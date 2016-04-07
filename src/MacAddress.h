5cab3fd95bb3f4f9c8736a4531e0462b3dffa4b6
David Beermann <mail@davidbeermann.com>
2016-04-07 11:12:08 +0200

    renamed repository for compatibility with Arduino

--- src/MacAddress.h
+++ src/MacAddress.h
@@ -4,11 +4,11 @@
  Copyright (c) 2016 Automatic Orchestra
 
  See the COPYRIGHT file at the top-level directory of this distribution and at:
- https://github.com/automatic-orchestra/library-for-arduino/blob/master/COPYRIGHT.
+ https://github.com/automatic-orchestra/ao_library/blob/master/COPYRIGHT.
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  This file is part of Automatic Orchestra. It is subject to the license terms
  in the LICENSE file found in the top-level directory of this distribution and at:
- https://github.com/automatic-orchestra/library-for-arduino/blob/master/LICENSE.
+ https://github.com/automatic-orchestra/ao_library/blob/master/LICENSE.
 
  No part of Automatic Orchestra, including this file, may be copied, modified,
  propagated, or distributed except according to the terms contained in the
